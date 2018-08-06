18c9bc49e6fe4a35bc0cc12646e269fd8d24ba8f
jmfveneroso <jmfveneroso@gmail.com>
2018-08-06 15:03:32 -0300

    Bidirectional RNN simplification.

--- bi_rnn.py
+++ bi_rnn.py
@@ -36,9 +36,7 @@ class RNN():
     np.random.seed(2)
     self.Waa = np.random.randn(n_a, n_a)
     self.Wax = np.random.randn(n_a, n_x)
-    self.Wya = np.random.randn(n_y, n_a)
     self.ba  = np.random.randn(n_a, 1  )
-    self.by  = np.random.randn(n_y, 1  )
 
   def rnn_cell_forward(self, xt, a_prev):
     a_next = np.tanh(np.dot(self.Wax, xt) + np.dot(self.Waa, a_prev) + self.ba)
@@ -93,56 +91,19 @@ class RNN():
     self.ba  -= learning_rate * np.clip(dba, -5.0, 5.0, dba)
     return dWax, dWaa, dba
 
-  def fit(self, X, Y, learning_rate=0.01):
-    epochs = 50
-    decay = learning_rate / epochs
-    for i in range(0, epochs):
-      learning_rate *= (1. / (1. + decay * i))
-      a, y_pred, caches = self.rnn_forward(X)
-  
-      # Cross entropy loss.
-      loss = -np.sum(Y * np.log(y_pred))
-      predicted_labels = np.argmax(y_pred, axis=0)
-      accuracy = 0
-      for i in range(len(labels)):
-        if predicted_labels[i] == labels[i]:
-          accuracy += 1.0
-      accuracy = accuracy / len(labels)
-      print('Cross entropy loss: ' + str(loss) + ', Accuracy: ' + str(accuracy))
-  
-      da = np.zeros((self.n_a, self.t))
-      dWya = np.zeros((self.n_y, self.n_a))
-      dby  = np.zeros((self.n_y, 1))
-      for i in range(t):
-        dsoftmax = -Y[:,i,np.newaxis] * (1 - y_pred[:,i,np.newaxis])
-        dbyt     = dsoftmax
-        dWyat    = np.dot(dsoftmax, a[:,i,np.newaxis].T)
-        da[:,i]  = np.ravel(np.dot(self.Wya.T, dsoftmax))
-        dby  += dbyt
-        dWya += dWyat
-  
-      dWax, dWaa, dba = self.rnn_backward(da, caches)
-  
-      # Update weights.
-      self.Wax -= learning_rate * np.clip(dWax, -5.0, 5.0, dWax)
-      self.Waa -= learning_rate * np.clip(dWaa, -5.0, 5.0, dWaa)
-      self.Wya -= learning_rate * np.clip(dWya, -5.0, 5.0, dWya)
-      self.ba  -= learning_rate * np.clip(dba, -5.0, 5.0, dba)
-      self.by  -= learning_rate * np.clip(dby, -5.0, 5.0, dby)
-    return y_pred
-
 class BiRNN():
   def __init__(self, n_x, n_y, n_a, t):
     self.n_x = n_x
     self.n_y = n_y
     self.n_a = n_a
     self.t   = t
+    np.random.seed(3)
     self.Wya = np.random.randn(n_y, n_a + n_a)
     self.by  = np.random.randn(n_y, 1  )
     self.f_rnn  = RNN(3, 2, 2, t)
     self.b_rnn = RNN(3, 2, 2, t)
 
-  def fit(self, X, Y, learning_rate=0.01, epochs=50):
+  def fit(self, X, Y, learning_rate=0.001, epochs=30):
     decay = learning_rate / epochs
     for i in range(0, epochs):
       learning_rate *= (1. / (1. + decay * i))
